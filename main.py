02601176d691b50589492a4934a76909a913e7a4
maxtk4 <67165280+maxtk4@users.noreply.github.com>
2023-07-12 12:00:34 -0700

    added comments and stuff

--- main.py
+++ main.py
@@ -36,8 +36,8 @@ count=0
 tracker=Tracker()
 
 # For long_range_b.mp4
-cy1=323 # Y-Coordinates for upper Line
-cy2=333 # Y-Coordinates for lower Line
+coord_y1=323 # Y-Coordinates for upper Line
+coord_y2=333 # Y-Coordinates for lower Line
 offset1=4 # Offset for upper Line
 offset2=6 # Offset for lower Line
 
@@ -48,92 +48,108 @@ vh_out = {} # Holds IDs of cars going out of frame for tracking
 counter_in = [] # List of IDs of cars that have gone into frame
 counter_out = [] # List of IDs of cars that have come out of frame
 
+#infinitely loop through the video
 while True:    
     ret,frame = cap.read()
+    #if the video is over, break out of the 'infinite' loop
     if not ret:
         break
+
+    #this code exists to limit the number of frames the code actually looks at
+    #counts the number of frames that have passed
     count += 1
+    #if the number of frames isn't a multiple of three, skip to the next frame
     if count % 3 != 0:
         continue
+
+    #resize the frame
     frame=cv2.resize(frame,(1020,500))
    
 
+    #run YOLOv8 on the frame
     results=model.predict(frame)
- #   print(results)
-    a=results[0].boxes.boxes
+    #print(results)
+    #get the data from the classification
+    a=results[0].boxes.data
+    #create a dataframe of the results
+    #why the f**k does it use a two letter variable I hate this
     px=pd.DataFrame(a).astype("float")
-#    print(px)
+    #print(px)
     list=[]
              
     # Object Tracker
+
+    #this loop selects all of the objects that are classes we care about, and adds the coordinates to the object tracking list
     for index,row in px.iterrows():
-#        print(row)
- 
+        
+        #get the coordinates of the bounding box
         x1=int(row[0])
         y1=int(row[1])
         x2=int(row[2])
         y2=int(row[3])
+
+        #I don't know what this is for and one letter variables certainly don't help --___--
         d=int(row[5])
+        #get the class of the object
         c=class_list[d]
+
         # Define what classes of objects to look for and record coordinates
         # Add more if needed (stick to streetside objects)
-        if 'car' in c:
-            list.append([x1,y1,x2,y2])
-        if 'truck' in c:
-            list.append([x1,y1,x2,y2])
-        if 'bus' in c:
-            list.append([x1,y1,x2,y2])
-        if 'bicycle' in c:
-            list.append([x1,y1,x2,y2])
-        if 'motorcycle' in c:
+        relevant_classes = ['car', 'truck', 'bus', 'bicycle', 'motorcycle']
+
+        if c in relevant_classes:
             list.append([x1,y1,x2,y2])
             
     # Finds the midpoint of the bounding box        
     bbox_id=tracker.update(list)
     for bbox in bbox_id:
         x3,y3,x4,y4,id=bbox
-        cx=int(x3+x4)//2
-        cy=int(y3+y4)//2
+        current_center_x=int(x3+x4)//2
+        current_center_y=int(y3+y4)//2
         
-    # Counting vehicles going "in" to frame
-        if cy1 < (cy+offset1) and cy1 > (cy-offset1):
-            vh_in[id] = cy
+        # Counting vehicles going "in" to frame
+        if coord_y1 < (current_center_y+offset1) and coord_y1 > (current_center_y-offset1):
+            vh_in[id] = current_center_y
         if id in vh_in:
-            if cy2 < (cy+offset2) and cy2 > (cy-offset2):
-                cv2.circle(frame,(cx,cy),4,(0,0,255),-1) # Draw circle
-                cv2.putText(frame,str(id),(cx,cy),cv2.FONT_HERSHEY_COMPLEX,0.8,(0,255,255),2) # Give and Print ID
+            if coord_y2 < (current_center_y+offset2) and coord_y2 > (current_center_y-offset2):
+                cv2.circle(frame,(current_center_x,current_center_y),4,(0,0,255),-1) # Draw circle
+                cv2.putText(frame,str(id),(current_center_x,current_center_y),cv2.FONT_HERSHEY_COMPLEX,0.8,(0,255,255),2) # Give and Print ID
                 if id not in counter_in:
                     counter_in.append(id)
                     
-    # Counting vehicles going "out" of frame
-        if cy2 < (cy+offset2) and cy2 > (cy-offset2):
-            vh_out[id] = cy
+        # Counting vehicles going "out" of frame
+        if coord_y2 < (current_center_y+offset2) and coord_y2 > (current_center_y-offset2):
+            vh_out[id] = current_center_y
         if id in vh_out:
-            if cy1 < (cy+offset1) and cy1 > (cy-offset1):
-                cv2.circle(frame,(cx,cy),4,(0,0,255),-1) # Draw circle
-                cv2.putText(frame,str(id),(cx,cy),cv2.FONT_HERSHEY_COMPLEX,0.8,(0,255,255),2) # Give and Print ID
+            if coord_y1 < (current_center_y+offset1) and coord_y1 > (current_center_y-offset1):
+                cv2.circle(frame,(current_center_x,current_center_y),4,(0,0,255),-1) # Draw circle
+                cv2.putText(frame,str(id),(current_center_x,current_center_y),cv2.FONT_HERSHEY_COMPLEX,0.8,(0,255,255),2) # Give and Print ID
                 if id not in counter_out:
                     counter_out.append(id)
         
         
-# For long_range_b.mp4
-    cv2.line(frame,(184,cy1),(814,cy1),(255,255,255),1) # X-Coordinates for upper Line
+    # For long_range_b.mp4
+    cv2.line(frame,(184,coord_y1),(814,coord_y1),(255,255,255),1) # X-Coordinates for upper Line
     cv2.putText(frame,('1Line'),(184,318),cv2.FONT_HERSHEY_COMPLEX_SMALL,0.8,(0,255,255),2) # Adds text above upper Line
     
-    cv2.line(frame,(10,cy2),(1007,cy2),(255,255,255),1) # X-Coordinates for lower Line
+    cv2.line(frame,(10,coord_y2),(1007,coord_y2),(255,255,255),1) # X-Coordinates for lower Line
     cv2.putText(frame,('2Line'),(1,331),cv2.FONT_HERSHEY_COMPLEX_SMALL,0.8,(0,255,255),2)
     
-# General Code
+    # General Code
+    #gets the number of cars in and out by counting the length of the arrays
     cin = (len(counter_in))
     cout = (len(counter_out))
     
-    
+    #displays the counts of cars in and out using openCV
     cv2.putText(frame,('In: ')+str(cin),(60,40),cv2.FONT_HERSHEY_COMPLEX_SMALL,0.8,(0,255,255),2)
     cv2.putText(frame,('Out: ')+str(cout),(60,20),cv2.FONT_HERSHEY_COMPLEX_SMALL,0.8,(0,255,255),2)
     
+    #shows the images
     cv2.imshow("RGB", frame)
     if cv2.waitKey(1)&0xFF==27:
         break
+
+#close the display window
 cap.release()
 cv2.destroyAllWindows()
 
