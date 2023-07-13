82938f84e76a157e500ff3fff4570007312f9f3b
maxtk4 <67165280+maxtk4@users.noreply.github.com>
2023-07-13 15:09:39 -0700

    clarified some stuff in the encoding

--- encode.py
+++ encode.py
@@ -4,16 +4,16 @@ import numpy as np
 #read in the csv file
 file = "data.csv"
 
-gray_code = {}
-for i in range(0, 1<<5):
-    gray=i^(i>>1)
-    gray_code[i] = "{0:0{1}b}".format(gray,5)
-
-normal_map = {0: '0', 1: '1'}
-reverse_map = {0: '1', 1: '0'}
-
 
 def encode(filename, flipped=False):
+    """
+    This function takes in the filename of a CSV (needs to have headers) with columns for time and event type
+
+    Reads into a dataframe
+    Iterates through the rows, and then
+    creates data from this by taking the times and classes
+    then converts to a binary string
+    """
     df = pd.read_csv(filename)
 
     shape = df.shape
@@ -22,6 +22,14 @@ def encode(filename, flipped=False):
     unique_dict = {}
     for index, event_type in enumerate({c: df[c].unique() for c in df}['event']):
         unique_dict[event_type] = index
+
+    gray_code = {}
+    for i in range(0, 1<<5):
+        gray=i^(i>>1)
+        gray_code[i] = "{0:0{1}b}".format(gray,5)
+
+    normal_map = {0: '0', 1: '1'}
+    reverse_map = {0: '1', 1: '0'}
 
 
     code = ''
